cb909092959534ea6cdee3a81f637a00f9ec3761
hanan.p@webandcrafts.com <hanan.p@webandcrafts.com>
2024-04-30 00:45:12 +0530

    added responsive in product list page

--- src/common/components/ProductOfferBanner/index.js
+++ src/common/components/ProductOfferBanner/index.js
@@ -4,9 +4,28 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const OfferBanner = () => {
+  const breakpoints = {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  };
   return (
     <div className="offer-slider">
-      <Swiper className="mySwiper" slidesPerView={2} spaceBetween={30}>
+      <Swiper
+        className="mySwiper"
+        slidesPerView={2}
+        spaceBetween={30}
+        breakpoints={breakpoints}
+      >
         <SwiperSlide>
           <img src={offer_banner} alt="offer_banner" />
         </SwiperSlide>
